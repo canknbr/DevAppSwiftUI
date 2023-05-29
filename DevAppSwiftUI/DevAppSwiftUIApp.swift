1383f8b70399aa3586544bd4dea7d87dbd427c2f
canknbr <cankanbur1@gmail.com>
2023-05-29 13:21:45 +0300

    Initial Commit

---
+++ DevAppSwiftUI/DevAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DevAppSwiftUIApp.swift
+//  DevAppSwiftUI
+//
+//  Created by Can Kanbur on 29.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct DevAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
